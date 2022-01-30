17927376302d0635878793586a1cc8346f2614d0
heyongsheng <1378431028@qq.com>
2022-01-30 17:35:49 +0800

    修改打包配置目录

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+/*
+ * @Author: 贺永胜
+ * @Date: 2022-01-09 22:38:36
+ * @email: 1378431028@qq.com
+ * @LastEditors: 贺永胜
+ * @LastEditTime: 2022-01-19 01:04:39
+ * @Descripttion: 
+ */
+module.exports = {
+  publicPath: "./", //打包后部署在一个子路径上http:xxx/m/
+  productionSourceMap: false
+};
